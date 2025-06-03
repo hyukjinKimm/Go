e2789fa281345f38a90ba08375c55983a2ee0301
hyukjin <hyukjin.kim.dev@gmail.com>
2025-06-03 20:33:31 +0900

    exam 폴더 추가

---
+++ exam/operationEx1/operationEx1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 10
+	num2 := 20
+	num3 := 30
+	sum := num1 + num2 + num3
+	average := sum / 3
+
+	fmt.Println(sum, average)
+}
---
+++ exam/operationEx2/operationEx2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	val1 := 1.5
+	val2 := 2.5
+	val3 := 3.5
+
+	sum := val1 + val2 + val3
+	average := sum / 3
+	fmt.Println(sum, average)
+
+}

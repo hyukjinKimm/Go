b8898525d14495069379ccd4b81b6537cccbd16d
hyukjin <hyukjin.kim.dev@gmail.com>
2025-06-08 16:08:42 +0900

    paintNeeded 함수에서 다중 반환 값 사용

--- headfirst/ch3/ch3.1/ch3.1.go
+++ headfirst/ch3/ch3.1/ch3.1.go
@@ -9,7 +9,7 @@ func paintNeeded(width float64, height float64) float64 {
 func main() {
 	var amount, total float64
 	amount = paintNeeded(4.2, 3.0)
-	fmt.Printf("%0.2f liters needed\n", amount)
+	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
 	amount = paintNeeded(5.2, 3.5)
 	fmt.Printf("%0.2f liters needed\n", amount)
